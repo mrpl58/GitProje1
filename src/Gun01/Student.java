8fb0660ed0c6d0afe4919c6e39d33687ae6e12f5
LENOVO <mrplfinans@gmail.com>
2023-01-26 19:38:52 +0300

    Programda düzeltme yapıldı.

--- src/Gun01/Student.java
+++ src/Gun01/Student.java
@@ -2,7 +2,7 @@ package Gun01;
 
 public class Student {
     public static void main(String[] args) {
-        System.out.println("26 Ocak Saat 19.20");
+        System.out.println("26 Ocak Saat 19:20");
 
         System.out.println("Hava akşama doğru soğudu");
 
